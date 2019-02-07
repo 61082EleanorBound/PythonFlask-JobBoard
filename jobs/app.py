7c436a6b8adf582898e5be5570f4fd019f8814d7
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-02-07 16:29:21 +0000

    completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,21 +1,21 @@
 import sqlite3
-from flask import Flask, render_template, g
-# created a constant
+from flask import Flask, render_template, g #g is to help provide access to the database
+#  a constant that contains the path to the already created database
 PATH = 'db/jobs.sqlite'
 
 #created instance of flask
 app = Flask(__name__)
 
-def open_connection():
+def open_connection(): # new function
     connection = getattr(g,'_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factor = sqlite3.Row
+    connection.row_factory = sqlite3.Row #all rows returned from the database will be named tuples
     return connection
 
-def execute_sql(sql, values, commit=False, single=False):
-    connection = open_connection()
-    cursor = connection.exectue(sql,values)
+def execute_sql(sql, values=(), commit=False, single=False):   # 4 parameters
+    connection = open_connection() 
+    cursor = connection.exectue(sql,values) # curser = the return value
     if commit==True:
         results = connection.commit()
     else:
